91c4e95ed9789e3186139f5d82d707cd189167a9
Hugo Cerda Minaya <hugo_cer@hotmail.com>
2022-07-27 02:21:58 -0500

    separación de métodos de autentificación de usuario

---
+++ herramienta_nous_template/passport/local-auth.js
@@ -0,0 +1,47 @@
+const {ubicacion} = require('../paths');
+const yaml = require('js-yaml');
+const fs=require('fs');
+const passport = require('passport');
+const PassportLocal = require('passport-local').Strategy;
+const bcrypt=require('bcryptjs');
+
+//Se usará cuando esté habilitado el registro de usuarios
+const encrypt = async(textPlain) => {
+    const hash = await bcrypt.hash(textPlain,10);
+    return hash;
+}
+  
+const compare = async(passwordPlain, hashPassword) => {
+    return await bcrypt.compare(passwordPlain, hashPassword)
+}
+
+const readJSON = (username, password) => {
+    const data = yaml.load(fs.readFileSync(ubicacion.usuarios, 'utf-8'));
+    try {
+      for(let i = 0; i < data.usuarios.length; i++) {
+        if (data.usuarios[i].usuario == username && compare(password, data.usuarios[i].contrasena)){
+          return data.usuarios[i];
+        }
+      }
+      return false;
+    } catch (error) {
+      fs.writeFileSync(ubicacion.usuarios, yaml.dump(data));
+    }
+  };
+
+// Serialización
+passport.serializeUser(function(user, done){
+    done(null, {id:user.id, nombre:user.nombre, rol:user.rol});
+});
+  
+//Deserealización
+passport.deserializeUser(function(id, done){
+    done(null, {id: 1, nombre: "Sistema", rol: "Administrativo"});
+});
+
+passport.use('local-signin', new PassportLocal(function(username,password,done){
+    let validacion = readJSON(username, password);
+    if(validacion)
+      return done(null, {id: validacion.id, nombre: validacion.nombre, rol:validacion.rol});
+    done(null, false, req.flash('signinMessage','Usuario no encontrado.'));
+  }));
---
+++ herramienta_nous_template/paths.js
@@ -0,0 +1,11 @@
+//Exportar módulo
+module.exports = {
+  ubicacion:{
+    usuarios: './database/usuarios.yml',
+    build_index: './views/index_build.ejs',
+    formulario_blog: './schemas/blog.schema.yml',
+    formulario_sitio: './schemas/nous_static_web.schema.yml',
+    contenidos_blog: './contenidos/blog',
+    contenidos_sitio: './contenidos/nous_static_web'
+  }
+};
