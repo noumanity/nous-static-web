070c694e94985d76b6acbb7d5978e8c2ad5242ca
Hugo Cerda Minaya <hugo_cer@hotmail.com>
2022-07-29 20:09:45 -0500

    Agrega descripción a titulo y fecha de la página en el formulario de captura de contenido

--- herramienta_nous_template/paths.js
+++ herramienta_nous_template/paths.js
@@ -5,7 +5,6 @@ module.exports = {
     build_index: './views/index_build.ejs',
     formulario_blog: './schemas/blog.schema.yml',
     formulario_sitio: './schemas/nous_static_web.schema.yml',
-    placeholder_formulario_sitio: './schemas/nous_static_web.form.yml',
     contenidos_blog: './contenidos/blog',
     contenidos_sitio: './contenidos/nous_static_web',
     esquema_contenido_sitio: './contenidos/nous_static_web/pagina_9f36cac5d1.yml',
